3d7f6200dadb8a127c519180cb2538732eeff22e
JasserToubib <jassertoubib@gmail.com>
2022-02-05 14:27:37 +0000

    edit Custom js to sales order

--- saleh/public/js/sales_order.js
+++ saleh/public/js/sales_order.js
@@ -2,14 +2,26 @@ frappe.ui.form.on("Sales Order", {
 	setup: function(frm) {
 		
 		frm.set_query("custom_payment_entry", function() {
-			//if (!frm.doc.sub_customer){
+			if (!frm.doc.sub_customer){
 				return {
 					filters: [
 						["Payment Entry","party", "=", frm.doc.customer],
 						["Payment Entry","unallocated_amount",">=",1000],
+						["Payment Entry","payment_type","=","Receive"],
+						["Payment Entry","docstatus","!=",2]
 					]
 				}
-			//}
+			}else{
+				return {
+                                        filters: [
+                                                ["Payment Entry","party", "=", frm.doc.sub_customer],
+                                                ["Payment Entry","unallocated_amount",">=",1000],
+                                                ["Payment Entry","payment_type","=","Receive"],
+                                                ["Payment Entry","docstatus","!=",2]
+                                        ]
+                                }
+			
+			}
 		});
 	}
 });
