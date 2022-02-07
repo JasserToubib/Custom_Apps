ddf50c540426c03000f239d0a03b6c3409192d36
JasserToubib <jassertoubib@gmail.com>
2022-02-07 14:56:08 +0000

    add some fixs in json,js files 9

--- saleh/public/js/customer.js
+++ saleh/public/js/customer.js
@@ -1,5 +1,21 @@
 frappe.ui.form.on('Customer', {
- validate: function(frm){
+    customer_type: function(frm){
+        if (frm.doc.customer_type == "Company") {
+            frm.set_df_property('tax_id', 'reqd', 1);
+        }else{
+            frm.set_df_property('tax_id', 'reqd', 0);
+        }
+        frm.refresh_field('tax_id');
+    },
+    onload: function(frm){
+        if (frm.doc.customer_type == "Company") {
+            frm.set_df_property('tax_id', 'reqd', 1);
+        }else{
+            frm.set_df_property('tax_id', 'reqd', 0);
+        }
+        frm.refresh_field('tax_id');
+    },
+    validate: function(frm){
         console.log("hello");
         var regex = /[^0-9]/g;
         if (regex.test(frm.doc.custom_customer_mobile) === true){
