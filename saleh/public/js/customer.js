e3fa5077025fd8c4d6bfdabd84e556ac3c0960b4
JasserToubib <jassertoubib@gmail.com>
2022-02-07 14:15:28 +0000

    add some fixs in json,js files 3

--- saleh/public/js/customer.js
+++ saleh/public/js/customer.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on('Customer', {
     validate: function(frm){
+	console.log("hello");
 	var regex = /[^0-9]/g;
 	if (regex.test(frm.doc.custom_customer_mobile) === true){
 		msgprint(__("Customer Mobile: Only numbers are allowed."));
