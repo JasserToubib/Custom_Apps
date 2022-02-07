20de0f93f88609066acb4abb9b58fc6d1451abfc
JasserToubib <jassertoubib@gmail.com>
2022-02-07 14:46:38 +0000

    add some fixs in json,js files 8

--- saleh/public/js/customer.js
+++ saleh/public/js/customer.js
@@ -1,9 +1,11 @@
-frappe.ui.form.on('Customer', 'validate',function(frm){
-	console.log("hello");
-	var regex = /[^0-9]/g;
-	if (regex.test(frm.doc.custom_customer_mobile) === true){
-		msgprint(__("Customer Mobile: Only numbers are allowed."));
-		validated = false;
-	}
+frappe.ui.form.on('Customer', {
+ validate: function(frm){
+        console.log("hello");
+        var regex = /[^0-9]/g;
+        if (regex.test(frm.doc.custom_customer_mobile) === true){
+                msgprint(__("Customer Mobile: Only numbers are allowed."));
+                validated = false;
+        }
+
     }
 });
