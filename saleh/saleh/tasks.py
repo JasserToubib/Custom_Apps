45ccd06d07ca09c87cb65c589821f11d8036b149
JasserToubib <jassertoubib@gmail.com>
2022-02-07 10:40:48 +0000

    Fix cron job for vehicle state

--- saleh/saleh/tasks.py
+++ saleh/saleh/tasks.py
@@ -7,9 +7,20 @@ def change_sales_order_statues():
 	draft_sales_orders = frappe.get_all("Sales Order",fields=["name","creation","customer"],filters={"status":"Draft"})
 	for sales_order in draft_sales_orders:
 		check_date=date_diff(frappe.utils.nowdate(), sales_order.creation)
-		if check_date >= 7:
-			check_customer= frappe.get_all("Customer",fields=["bank_customer"],filters={"name":sales_order.customer},limit=1)
-			if check_customer[0]["bank_customer"] != 0:
+		check_customer= frappe.get_all("Customer",fields=["bank_customer"],filters={"name":sales_order.customer},limit=1)
+		if check_customer[0]["bank_customer"] == 1:
+			if check_date >= 8:
+				get_serial_no=frappe.get_all("Sales Order Item",fields=["serial_no"],filters={"parent": sales_order.name,"parenttype": "Sales Order"},order_by= "idx")
+				for serial_no in get_serial_no:
+					if serial_no.serial_no != None:
+						get_reservation_status= frappe.get_all("Serial No",fields=["reservation_status"],filters={"name":serial_no.serial_no})
+						if get_reservation_status[0]["reservation_status"] == "Reserved":
+							doc=frappe.get_doc("Serial No",serial_no.serial_no)
+							doc.reservation_status = "Available"
+							doc.save()
+							frappe.db.commit()
+		else:
+			if check_date >= 3:
 				get_serial_no=frappe.get_all("Sales Order Item",fields=["serial_no"],filters={"parent": sales_order.name,"parenttype": "Sales Order"},order_by= "idx")
 				for serial_no in get_serial_no:
 					if serial_no.serial_no != None:
