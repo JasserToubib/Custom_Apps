eea7c3d41a3c3d03a0dfde21aab3deef94d40e5b
JasserToubib <jassertoubib@gmail.com>
2022-02-08 10:36:32 +0000

    Fix salesorder, delivery note customer mobile 2

--- saleh/public/js/deliver_note.js
+++ saleh/public/js/deliver_note.js
@@ -31,7 +31,9 @@ frappe.ui.form.on('Delivery Note', {
                         'callback': function(res){
                                 if (res.message.custom_customer_mobile){
                                         frm.set_value("custom_customer_mobile", res.message.custom_customer_mobile || '');
-                                }
+                                }else{
+					frm.set_value("custom_customer_mobile", '');
+				}
                         }
                 });
                 //frm.add_fetch('sub_customer','custom_customer_mobile','custom_customer_mobile');
