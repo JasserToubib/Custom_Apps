3745f0e6e3c6edbdf39911e08894481bdebdc49e
JasserToubib <jassertoubib@gmail.com>
2022-02-14 12:58:24 +0000

    Add python script for sales order

---
+++ saleh/overrides/sales_order.py
@@ -0,0 +1,11 @@
+from erpnext.selling.doctype.sales_order.sales_order import SalesOrder
+
+class CustomSalesOrder(SalesOrder):
+
+	def before_save(self):
+		super(SalesOrder, self).before_save()
+		if self.sub_customer:
+			cutom_num=frappe.get_all("Customer",fields=["custom_customer_mobile"],filters={"name": self.sub_customer})
+			if len(num) >= 1 :
+				self.custom_customer_mobile=frappe.get_all("Customer",fields=["custom_customer_mobile"],filters={"name": self.sub_customer})[0].custom_customer_mobile
+
